d4ca772cf84c8e2949497f7d26c65e7aae1a37ac
Louis Saglio <louis.saglio@dedomainia.com>
2025-12-13 21:09:13 +0100

    Median of sorted arrays 4 -- O((n+m)/2)

--- src/median_of_sorted_arrays_4.rs
+++ src/median_of_sorted_arrays_4.rs
@@ -1,49 +1,66 @@
-    #[allow(dead_code)]
+#[allow(dead_code)]
 fn median_of_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
-    let mut merged_array = Vec::new();
+    let mid_array_index = (nums1.len() + nums2.len()) / 2;
+    let is_even = (nums1.len() + nums2.len()) % 2 == 0;
     let mut index_1 = 0;
     let mut index_2 = 0;
-    if nums1.len() == 0 {
-        merged_array = nums2;
-    } else if nums2.len() == 0 {
-        merged_array = nums1;
-    } else {
-        loop {
-            let item_1 = nums1[index_1];
-            let item_2 = nums2[index_2];
-            if item_1 <= item_2 {
-                merged_array.push(item_1);
+    let mut previous: Option<i32> = None;
+    loop {
+        let item_1 = nums1.get(index_1);
+        let item_2 = nums2.get(index_2);
+        match (item_1, item_2) {
+            (Some(item_1), Some(item_2)) => {
+                let item = if item_1 <= item_2 {
+                    index_1 += 1;
+                    item_1
+                } else {
+                    index_2 += 1;
+                    item_2
+                };
+                if index_1 + index_2 - 1 == mid_array_index {
+                    return if is_even {
+                        ((*item + previous.expect("Unreachable")) as f64) / 2.0
+                    } else {
+                        *item as f64
+                    }
+                }
+                previous = Some(*item);
+            }
+            (Some(item_1), None) => {
                 index_1 += 1;
+                if index_1 + index_2 - 1 == mid_array_index {
+                    return if is_even {
+                        ((*item_1 + previous.expect("Unreachable")) as f64) / 2.0
+                    } else {
+                        *item_1 as f64
+                    }
+                }
+                previous = Some(*item_1);
             }
-            if item_2 <= item_1 {
-                merged_array.push(item_2);
+            (None, Some(item_2)) => {
                 index_2 += 1;
-            }
-            if index_1 == nums1.len() {
-                for item in nums2[index_2..].iter() {
-                    merged_array.push(*item);
+                if index_1 + index_2 - 1 == mid_array_index {
+                    return if is_even {
+                        ((*item_2 + previous.expect("Unreachable")) as f64) / 2.0
+                    } else {
+                        *item_2 as f64
+                    }
                 }
-                break;
+                previous = Some(*item_2);
             }
-            if index_2 == nums2.len() {
-                for item in nums1[index_1..].iter() {
-                    merged_array.push(*item);
-                }
-                break;
+            (None, None) => {
+                unreachable!()
             }
         }
-    }
-    let mid_array_index = merged_array.len() / 2;
-    if merged_array.len() % 2 == 0 {
-        (merged_array[mid_array_index - 1] + merged_array[mid_array_index]) as f64 / 2.0
-    } else {
-        merged_array[mid_array_index] as f64
     }
 }
 
 #[test]
 fn test_median_of_sorted_arrays() {
+    assert_eq!(median_of_sorted_arrays(vec![5], vec![]), 5.0);
     assert_eq!(median_of_sorted_arrays(vec![1, 3], vec![2]), 2.0);
+    assert_eq!(median_of_sorted_arrays(vec![1, 3, 4], vec![2, 5]), 3.0);
+    assert_eq!(median_of_sorted_arrays(vec![1, 3, 4, 5], vec![2]), 3.0);
+    assert_eq!(median_of_sorted_arrays(vec![3, 4], vec![2, 5]), 3.5);
     assert_eq!(median_of_sorted_arrays(vec![3], vec![2, 4, 5]), 3.5);
-    assert_eq!(median_of_sorted_arrays(vec![3], vec![]), 3.0);
 }
