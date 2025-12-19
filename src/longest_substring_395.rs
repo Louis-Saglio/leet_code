23c975ef9adcfbb613d72ab6188eb51111ec5b1e
Louis Saglio <louis.saglio@proton.me>
2025-12-19 11:13:02 +0100

    Longest Substring 395 -- Update function signature and adjust types to match with the standard leetcode signature

--- src/longest_substring_395.rs
+++ src/longest_substring_395.rs
@@ -1,8 +1,8 @@
 use std::collections::{HashMap, HashSet};
 
 #[allow(dead_code)]
-fn longest_substring_395(s: &str, k: usize) -> usize {
-    let mut char_freq: HashMap<_, usize> = HashMap::new();
+fn longest_substring(s: String, k: i32) -> i32 {
+    let mut char_freq: HashMap<_, i32> = HashMap::new();
     for c in s.chars() {
         char_freq.entry(c).and_modify(|v| *v += 1).or_insert(1);
     }
@@ -12,12 +12,12 @@ fn longest_substring_395(s: &str, k: usize) -> usize {
         .map(|(k, _)| *k)
         .collect::<HashSet<_>>();
     if too_rare_chars.is_empty() {
-        s.len()
+        s.len() as i32
     } else {
         s
             .split(|it| too_rare_chars.contains(&it))
-            .filter(|it| it.len() >= k)
-            .map(|it| longest_substring_395(it, k))
+            .filter(|it| it.len() >= k as usize)
+            .map(|it| longest_substring(it.to_string(), k))
             .max()
             .unwrap_or(0)
     }
@@ -25,16 +25,16 @@ fn longest_substring_395(s: &str, k: usize) -> usize {
 
 #[cfg(test)]
 mod tests {
-    use crate::longest_substring_395::longest_substring_395;
+    use crate::longest_substring_395::longest_substring;
 
     #[test]
     fn test1_395() {
-        assert_eq!(longest_substring_395("ababbc", 2), 5);
-        assert_eq!(longest_substring_395("aaabb", 3), 3);
-        assert_eq!(longest_substring_395("abcabcbb", 3), 0);
-        assert_eq!(longest_substring_395("dabcabcbb", 2), 8);
-        assert_eq!(longest_substring_395("dabbaebcbb", 2), 4);
-        assert_eq!(longest_substring_395("bbbbb", 1), 5);
-        assert_eq!(longest_substring_395("pwwkew", 2), 2);
+        assert_eq!(longest_substring("ababbc".into(), 2), 5);
+        assert_eq!(longest_substring("aaabb".into(), 3), 3);
+        assert_eq!(longest_substring("abcabcbb".into(), 3), 0);
+        assert_eq!(longest_substring("dabcabcbb".into(), 2), 8);
+        assert_eq!(longest_substring("dabbaebcbb".into(), 2), 4);
+        assert_eq!(longest_substring("bbbbb".into(), 1), 5);
+        assert_eq!(longest_substring("pwwkew".into(), 2), 2);
     }
 }
