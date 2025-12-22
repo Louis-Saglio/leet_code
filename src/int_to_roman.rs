b72aeb72490c90340da9025f2df2b905acfc0010
Louis Saglio <louis.saglio@proton.me>
2025-12-22 18:42:56 +0100

    Int to roman 12 -- Remove heap usage

--- src/int_to_roman.rs
+++ src/int_to_roman.rs
@@ -65,52 +65,64 @@ Constraints:
 
 **/
 
-fn convert_decimal(val: i32, unit: &str, five: &str, ten: &str) -> String {
+fn convert_decimal(val: i32, unit: u8, five: u8, ten: u8) -> [Option<u8>; 4] {
     match val {
-        0 => "".to_string(),
-        1 => format!("{}", unit),
-        2 => format!("{}{}", unit, unit),
-        3 => format!("{}{}{}", unit, unit, unit),
-        4 => format!("{}{}", unit, five),
-        5 => format!("{}", five),
-        6 => format!("{}{}", five, unit),
-        7 => format!("{}{}{}", five, unit, unit),
-        8 => format!("{}{}{}{}", five, unit, unit, unit),
-        9 => format!("{}{}", unit, ten),
+        0 => [None, None, None, None],
+        1 => [Some(unit), None, None, None],
+        2 => [Some(unit), Some(unit), None, None],
+        3 => [Some(unit), Some(unit), Some(unit), None],
+        4 => [Some(unit), Some(five), None, None],
+        5 => [Some(five), None, None, None],
+        6 => [Some(five), Some(unit), None, None],
+        7 => [Some(five), Some(unit), Some(unit), None],
+        8 => [Some(five), Some(unit), Some(unit), Some(unit)],
+        9 => [Some(unit), Some(ten), None, None],
         _ => unreachable!(),
     }
 }
 
 #[allow(dead_code)]
 pub fn int_to_roman(num: i32) -> String {
-    let mut result = String::new();
+    let (I, V, X, L, C, D, M) = (
+        "I".as_bytes()[0],
+        "V".as_bytes()[0],
+        "X".as_bytes()[0],
+        "L".as_bytes()[0],
+        "C".as_bytes()[0],
+        "D".as_bytes()[0],
+        "M".as_bytes()[0],
+    );
+    let mut result: [Option<u8>; 16] = [None; 16];
     let mut num = num;
     let val = num / 1000;
-    let thousands = match val {
-        0 => "",
-        1 => "M",
-        2 => "MM",
-        3 => "MMM",
+    let thousands: [Option<u8>; 4] = match val {
+        0 => [None, None, None, None],
+        1 => [Some(M), None, None, None],
+        2 => [Some(M), Some(M), None, None],
+        3 => [Some(M), Some(M), Some(M), None],
         _ => unreachable!(),
     };
-    result.push_str(thousands);
+    result[..4].copy_from_slice(&thousands);
     num -= val * 1000;
-    let (result, _) = [
-        (100, "C", "D", "M"),
-        (10, "X", "L", "C"),
-        (1, "I", "V", "X"),
-    ]
-    .into_iter()
-    .fold(
-        (result, num),
-        |(result, num), (magnitude, unit, five, ten)| {
-            let decimal = num / magnitude;
-            let result = format!("{}{}", result, convert_decimal(decimal, unit, five, ten));
-            let num = num - decimal * magnitude;
-            (result, num)
-        },
-    );
-    result
+    let (result, _) = [(100, C, D, M), (10, X, L, C), (1, I, V, X)]
+        .into_iter()
+        .fold(
+            (result, num),
+            |(mut result, num), (magnitude, unit, five, ten)| {
+                let decimal = num / magnitude;
+                let roman = convert_decimal(decimal, unit, five, ten);
+                let (start, end) = match magnitude {
+                    1 => (12, 16),
+                    10 => (8, 12),
+                    100 => (4, 8),
+                    _ => unreachable!(),
+                };
+                result[start..end].copy_from_slice(&roman);
+                let num = num - decimal * magnitude;
+                (result, num)
+            },
+        );
+    String::from_utf8(result[..16].iter().filter_map(|&c| c).collect::<Vec<_>>()).unwrap()
 }
 
 #[test]
