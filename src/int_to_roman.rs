dfbdd57f990d7d5a3176e4128983215738a8fb0d
Louis Saglio <louis.saglio@proton.me>
2025-12-23 12:42:22 +0100

    Int to roman 12 -- Use index instead of Option

--- src/int_to_roman.rs
+++ src/int_to_roman.rs
@@ -65,18 +65,52 @@ Constraints:
 
 **/
 
-fn convert_decimal(val: i32, unit: u8, five: u8, ten: u8) -> [Option<u8>; 4] {
+fn convert_decimal(
+    val: i32,
+    unit: u8,
+    five: u8,
+    ten: u8,
+    buffer: &mut [u8; 15],
+    index: usize,
+) -> usize {
     match val {
-        0 => [None, None, None, None],
-        1 => [Some(unit), None, None, None],
-        2 => [Some(unit), Some(unit), None, None],
-        3 => [Some(unit), Some(unit), Some(unit), None],
-        4 => [Some(unit), Some(five), None, None],
-        5 => [Some(five), None, None, None],
-        6 => [Some(five), Some(unit), None, None],
-        7 => [Some(five), Some(unit), Some(unit), None],
-        8 => [Some(five), Some(unit), Some(unit), Some(unit)],
-        9 => [Some(unit), Some(ten), None, None],
+        0 => index,
+        1 => {
+            buffer[index] = unit;
+            index + 1
+        }
+        2 => {
+            buffer[index..index + 1].copy_from_slice(&[unit, unit]);
+            index + 2
+        }
+        3 => {
+            buffer[index..index + 3].copy_from_slice(&[unit, unit, unit]);
+            index + 3
+        }
+        4 => {
+            buffer[index..index + 2].copy_from_slice(&[unit, five]);
+            index + 2
+        }
+        5 => {
+            buffer[index] = five;
+            index + 1
+        }
+        6 => {
+            buffer[index..index + 2].copy_from_slice(&[five, unit]);
+            index + 2
+        }
+        7 => {
+            buffer[index..index + 3].copy_from_slice(&[five, unit, unit]);
+            index + 3
+        }
+        8 => {
+            buffer[index..index + 4].copy_from_slice(&[five, unit, unit, unit]);
+            index + 4
+        }
+        9 => {
+            buffer[index..index + 2].copy_from_slice(&[unit, ten]);
+            index + 2
+        }
         _ => unreachable!(),
     }
 }
@@ -92,37 +126,26 @@ pub fn int_to_roman(num: i32) -> String {
         "D".as_bytes()[0],
         "M".as_bytes()[0],
     );
-    let mut result: [Option<u8>; 16] = [None; 16];
+    let mut result = [0; 15];
+    let mut index: usize = 0;
     let mut num = num;
     let val = num / 1000;
-    let thousands: [Option<u8>; 4] = match val {
-        0 => [None, None, None, None],
-        1 => [Some(M), None, None, None],
-        2 => [Some(M), Some(M), None, None],
-        3 => [Some(M), Some(M), Some(M), None],
+    let thousands: &[u8] = match val {
+        0 => &[],
+        1 => &[M],
+        2 => &[M, M],
+        3 => &[M, M, M],
         _ => unreachable!(),
     };
-    result[..4].copy_from_slice(&thousands);
+    index += thousands.len();
+    result[..index].copy_from_slice(thousands);
     num -= val * 1000;
-    let (result, _) = [(100, C, D, M), (10, X, L, C), (1, I, V, X)]
-        .into_iter()
-        .fold(
-            (result, num),
-            |(mut result, num), (magnitude, unit, five, ten)| {
-                let decimal = num / magnitude;
-                let roman = convert_decimal(decimal, unit, five, ten);
-                let (start, end) = match magnitude {
-                    1 => (12, 16),
-                    10 => (8, 12),
-                    100 => (4, 8),
-                    _ => unreachable!(),
-                };
-                result[start..end].copy_from_slice(&roman);
-                let num = num - decimal * magnitude;
-                (result, num)
-            },
-        );
-    String::from_utf8(result[..16].iter().filter_map(|&c| c).collect::<Vec<_>>()).unwrap()
+    for (magnitude, unit, five, ten) in [(100, C, D, M), (10, X, L, C), (1, I, V, X)] {
+        let decimal = num / magnitude;
+        index = convert_decimal(decimal, unit, five, ten, &mut result, index);
+        num -= decimal * magnitude;
+    }
+    unsafe { String::from_utf8_unchecked(result[..index].to_vec()) }
 }
 
 #[test]
